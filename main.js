181239e3d8f30906a10610dc0c08c73eaaebb391
sadiaromi <sadiaroman28@gmail.com>
2024-05-29 19:46:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+var firstNumber = 4;
+var secondNumber = 3;
+console.log("The sum of first two numbers is:", addNumbers(firstNumber, secondNumber));
+var thirdNumber = 6;
+var fourthNumber = 4;
+console.log("The sum of the second two numbers is:", addNumbers(thirdNumber, fourthNumber));
---
+++ main.ts
@@ -0,0 +1,11 @@
+function addNumbers(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+let firstNumber = 4;
+let secondNumber = 3;
+console.log("The sum of first two numbers is:", addNumbers(firstNumber, secondNumber));
+
+let thirdNumber = 6;
+let fourthNumber = 4;
+console.log("The sum of the second two numbers is:", addNumbers(thirdNumber, fourthNumber));
